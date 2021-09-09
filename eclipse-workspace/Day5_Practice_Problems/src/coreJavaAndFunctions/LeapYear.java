3d1e9de8b11098389c9b89ebec2d2f8a63ea53cb
Grid112 <sumitware1999@gmail.com>
2021-09-09 22:20:26 +0530

    For Leap Year

---
+++ eclipse-workspace/Day5_Practice_Problems/src/coreJavaAndFunctions/LeapYear.java
@@ -0,0 +1,28 @@
+package coreJavaAndFunctions;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+
+        String Year;
+        System.out.println("Enter A Year: " );
+        Scanner Sc = new Scanner(System.in);
+        Year = Sc.nextLine();
+
+        if (Year.length() == 4) {
+            int leapYear = Integer.parseInt(Year);
+            System.out.println("Entered Year is:" +leapYear);
+
+            if((leapYear % 4) == 0 && (leapYear % 100) != 0 || (leapYear % 400) == 0){
+                System.out.println("Entered Year Is Leap Year");
+            }
+            else {
+                System.out.println("Entered Year Is Not A Leap Year");
+            }
+        }
+        else {
+            System.out.println("Invalid No! Enter A 4 Digit Number");
+        }
+    }
+}
